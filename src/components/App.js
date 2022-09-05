13cacd95aa0c51031249993b4e056306d6c64b98
prabha666 <108609740+prabha666@users.noreply.github.com>
2022-09-05 19:23:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
+  const handleInput1 = (event) =>{
+  const input1 = event.target.value
+    
+    console.log(input1)
+  }
+  const handleInput2 = (event) =>{
+    const input2 = event.target.value
 
-  const handleInput = (event) =>{
-   // use console.log
+    console.log(input2)
   }
 
   // do not change id of input elements
   return (
-    <div id="main">
+    <div id="main">      
       <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" type={'text'} />
+      <input id="text-input" type={'text'} onChange = {handleInput1}/>
 
       <br/>
       <br/>
 
       <label htmlFor='num-input'>Number input</label>
-      <input id="num-input"  type={'number'} />
+      <input id="num-input"  type={'number'} onChange = {handleInput2} />
       <br/>
     </div>
   )
@@ -24,3 +30,4 @@ const App = () => {
 
 
 export default App;
+
